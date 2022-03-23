45d7cdb080e55464cf366f92adb0be5b79843aa7
Jin-Lee123 <wls4310@koreatech.ac.kr>
2022-03-23 21:00:07 +0700

    125~135

---
+++ A132_Label/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A132_Label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string raffaello = "라파엘로 신차오, 이탈리아, 르테상스 3대 거장, 1483~1520";
+            string schoolOfAthens = "~~~~~~";
+
+            label1.Text = raffaello;
+            label2.Text = schoolOfAthens;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+    }
+}
